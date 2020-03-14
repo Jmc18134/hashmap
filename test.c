bfec5aa2abd637c07f2b3a6be324a38e09045635
james <tmb18134@uni.strath.ac.uk>
2020-03-14 14:22:43 +0000

    Cleaned up test

--- test.c
+++ test.c
@@ -6,18 +6,21 @@ int main(void) {
 
 	for (int i=0, j=100; i<100; i++, j--) {
 		if (hashmap_insert(hm, i, j)) {
-			printf("Error on inserting %d:%d\n", i, j);
+			fprintf(stderr, "Error on inserting %d:%d\n", i, j);
+		}
+	}
+
+	for (int i=25; i<75; i++) {
+		if (hashmap_remove(hm, i)) {
+			fprintf(stderr, "Error, key %d not in hashmap\n", i);
 		}
 	}
 
 	int res = 0;
-	for (int i=25; i<75; i++) {
-		hashmap_remove(hm, i);
-	}
-
 	for (int i=0; i<100; i++) {
 		int err = hashmap_get(hm, i, &res);
 		printf("%d:%d\n", i, err ? -69 : res);
+		res = 0;
 	}
 
 	destroy_hashmap(hm);
