36d7fb2e74f5da9a55c84beda1ea33139f53c6ad
james <tmb18134@uni.strath.ac.uk>
2020-03-14 13:41:11 +0000

    Add test

--- test.c
+++ test.c
@@ -2,6 +2,17 @@
 #include "hashmap.h"
 
 int main(void) {
-	printf("Hello, world!\n");
+	HashMap *hm = new_hashmap(intkey(), intval());
+	for (int i=0, j=0; i<100; i++, j--) {
+		hashmap_insert(hm, (void *)i, (void*)j);
+	}
+	for (int i=25; i<75; i++) {
+		hashmap_remove(hm, (void*)i);
+	}
+	for (int i=0; i<100; i++) {
+		int res = (int) hashmap_get(hm, i);
+		printf("%d:%d\n", res ? res : -69);
+	}
+	destroy_hashmap(hm);
 	return 0;
 }
